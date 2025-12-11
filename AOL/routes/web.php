2beb51985f222515408b9009393bee91ddfe1bf5
Davidkys <david.arifin@binus.ac.id>
2025-12-11 13:41:55 +0700

    FE: add register page

--- AOL/routes/web.php
+++ AOL/routes/web.php
@@ -12,11 +12,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
 Route::get('/auth/google', [AuthController::class, 'redirect'])->name('google.redirect');
 Route::get('/auth/google/callback', [AuthController::class, 'callback'])->name('google.callback');
 
-
-Route::get('/register/phone', [AuthController::class, 'registerPhonePage'])->name('register.phonePage');
-Route::post('/register/phone', [AuthController::class, 'registerPhone']);
-
-Route::get('/register/password', [AuthController::class, 'registerPasswordPage'])->name('register.passwordPage');
-Route::post('/register/password', [AuthController::class, 'registerPassword']);
+Route::get('/register', [AuthController::class, 'registerPage'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.attempt');
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
